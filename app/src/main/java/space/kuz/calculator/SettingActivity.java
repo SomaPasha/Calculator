7af987157ae2dc3b431dca8b0b7d4c22e96e4bc7
Ferpasha <pavel.kuzneczov.96@gmail.com>
2021-09-22 17:38:26 +0300

    Урок №5 Задание №1 (Cмена темы при нажатии - но недоработанно)

--- app/src/main/java/space/kuz/calculator/SettingActivity.java
+++ app/src/main/java/space/kuz/calculator/SettingActivity.java
@@ -1,10 +1,14 @@
 package space.kuz.calculator;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
@@ -17,19 +21,20 @@ import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class SettingActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         Switch switch_setting = (Switch) findViewById(R.id.setting_switch);
-
+        int isSettingTheme =  AppCompatDelegate.getDefaultNightMode();
         switch_setting.setOnClickListener(v ->  {
-
                 boolean checked = ((Switch) v).isChecked();
                 if (checked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                 }
                 else{
-
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 }
         });
     }
